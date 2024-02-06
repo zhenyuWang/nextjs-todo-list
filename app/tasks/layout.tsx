3b2f363bee769fed8bc4f8cfd15ac8d2ce4f00bb
zhenyuWang <13641039885@163.com>
2024-02-06 21:15:32 +0800

    feat: set default theme

--- app/tasks/layout.tsx
+++ app/tasks/layout.tsx
@@ -1,21 +1,34 @@
 'use client'
 
 import Sidebar from '@/app/components/Sidebar/Sidebar'
-import { useUser } from '@clerk/nextjs'
+import { useEffect } from 'react'
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
-  const { isSignedIn } = useUser()
+  type Theme = 'light' | 'dark'
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const localTheme = window.localStorage.getItem('theme') as Theme | null
+
+      if (localTheme) {
+        if (localTheme === 'dark') {
+          document.documentElement.classList.add('dark')
+        }
+      } else if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        document.documentElement.classList.add('dark')
+      }
+    }
+  }, [])
 
   return (
     <div className='h-[100vh] flex px-5 py-5 overflow-hidden'>
       <Sidebar />
-      {isSignedIn ? (<div className='h-full flex-1 bg-slate-700 border border-slate-500 rounded-md overflow-hidden'>
+      <div className='h-full flex-1 bg-slate-700 border border-slate-500 rounded-md overflow-hidden'>
         {children}
-      </div>) : <div className='w-full pt-10 text-center'>loading...</div>}
+      </div>
     </div>
   )
 }
